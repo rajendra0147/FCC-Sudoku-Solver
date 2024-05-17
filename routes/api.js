8ab71ae3883f75b6c757a5f8113ec34f958bdae9
Rajendra <rajendrathakur864@gmail.com>
2024-05-17 23:00:49 +0530

    first commit

--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,70 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      const {puzzle, coordinate, value} = req.body;
+      if(!puzzle || !coordinate || !value){
+        res.json({error: "Required field(s) missing"});
+      }
 
+      if(solver.validate(puzzle) != "Valid"){
+        res.json({error: solver.validate(puzzle)});
+        return;
+      }
+      const row = coordinate.split("")[0];
+      const column = coordinate.split("")[1];
+      if(coordinate.length !== 2 ||
+        !/[a-i]/i.test(row) ||
+        !/^[1-9]$/i.test(column)
+      ){
+        console.log("Invalid coordinate :>> ");
+        res.json({ error: 'Invalid coordinate'});
+        return;
+      }
+
+      if(!/^[1-9]$/.test(value)){
+        res.json({error: "Invalid value"});
+        return;
+      }
+
+      const index = (solver.letterToNumber(row) - 1) * 9 + (+column - 1);
+      if(puzzle[index] == value){
+        res.json({valid: true});
+        return;
+      }
+
+      let validCol = solver.checkColPlacement(puzzle, row, column, value);
+      let validRow = solver.checkRowPlacement(puzzle, row, column, value);
+      let validReg = solver.checkRegionPlacement(puzzle, row, column, value);
+      console.log(validReg)
+      let conflicts = [];
+      if(validCol && validReg && validRow){
+        res.json({valid: true});
+      }else{
+        if(!validRow){
+          conflicts.push("row");
+        }
+        if(!validCol){
+          conflicts.push("column");
+        }
+        if(!validReg){
+          conflicts.push("region");
+        }
+        res.json({valid:false, conflict:conflicts})
+      }
     });
     
   app.route('/api/solve')
     .post((req, res) => {
-
+      const {puzzle} = req.body;
+      if(solver.validate(puzzle) != "Valid"){
+        res.json({error: solver.validate(puzzle)});
+        return;
+      }
+      let solvedString = solver.solve(puzzle);
+      if(!solvedString){
+        res.json({error: "Puzzle cannot be solved"});
+      }else{
+        res.json({solution:solvedString})
+      }
     });
 };
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,63 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
-
+let solver = new Solver();
+let validPuzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
 suite('Unit Tests', () => {
-
+    suite('solver tests', ()=>{
+        test("Logic handles a valid puzzle string of 81 character", function(done){
+            let complete = '135762984946381257728459613694517832812936745357824196473298561581673429269145378';
+            assert.equal(solver.solve(validPuzzle), complete)
+            done();
+        })
+        test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", function(done){
+            let invalidPuzzle = '1.5..2.84..63.12.7.2..5.....9..1....Asdf.3674.3.7.2..9.47...8..1..16....926914.37.';
+            assert.equal(solver.solve(invalidPuzzle), false)
+            done()
+        })
+        test("Logic handles a puzzle string that is not 81 characters in length", function(done){
+            let invalidPuzzle = '1.5..2.84..63.12.7.2..5.....9..1....Asdf.3674.3.7.2..9.47...8..1..16....926914.37.123';
+            assert.equal(solver.solve(invalidPuzzle), false)
+            done()
+        })
+        test("Logic handles a valid row placement", function(done){
+            assert.equal(solver.checkRowPlacement(validPuzzle, 'A', '2', '9'), true)
+            done()
+        })
+        test("Logic handles an invalid row placement", function(done){
+            assert.equal(solver.checkRowPlacement(validPuzzle, 'A', '2', '1'), false)
+            done()
+        })
+        test("Logic handles a valid column placement", function(done){
+            assert.equal(solver.checkColPlacement(validPuzzle,'A','2','3'), true);
+            done();
+        })
+        test("Logic handles an invalid column placement", function(done){
+            assert.equal(solver.checkColPlacement(validPuzzle,'A','2','2'), false)
+            done()
+        })
+        test("Logic handles a valid region (3x3 grid) placement", function(done){
+            assert.equal(solver.checkRegionPlacement(validPuzzle,'A','2',3), true)
+            done()
+        })
+        test("Logic handles an invalid region (3x3 grid) placement", function(done){
+            assert.equal(solver.checkRegionPlacement(validPuzzle,'A','2','2'), false);
+            done();
+        })
+        test("Valid puzzle strings pass the solver", function(done){
+            let complete = '135762984946381257728459613694517832812936745357824196473298561581673429269145378';
+            assert.equal(solver.solve(validPuzzle), complete);
+            done()
+        })
+        test("Invalid puzzle strings fail the solver", function(done){
+            let invalidPuzzle = '115..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+            assert.equal(solver.solve(invalidPuzzle), false);
+            done()
+        })
+        test("Solver returns the expected solution for an incomplete puzzle", function(done){
+            let complete = '135762984946381257728459613694517832812936745357824196473298561581673429269145378';
+            assert.equal(solver.solve(validPuzzle), complete)
+            done()
+        })
+    })
 });
